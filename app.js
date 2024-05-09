622c4c2af8c6d4f4a6c225445aa3ba5ae19206db
Aman Ubhi <amanjot.ubhi.work@gmail.com>
2024-05-08 21:44:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let total = 0
 
 console.log(saveEl)
 
 function countIn () {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function saveCt () {
     let countStr = count + " - "
-    saveEl.innerText += countStr
-    console.log(count)
+    saveEl.textContent += countStr
+    total += count
+    countEl.textContent = 0
+    count = 0
+    totalCt()
+}
+
+function totalCt() {
+    totalEl.textContent = "Total: " + total
 }
